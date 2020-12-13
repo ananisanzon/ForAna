0e6caecffbef3f19c539565acd5dcf230e50bd6a
ananisanzon <ana.nisanzon@gmail.com>
2020-12-13 02:46:27 +0200

    Assert Lines 25-32

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,6 +1,10 @@
-import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class LoginPage extends BasePage {
+    private WebDriver driver;
     // Click on כניסה/הרשמה
     public void pressElement() throws InterruptedException {
         String clickEnterOrRegistry = "span[class=seperator-link]";
@@ -13,24 +17,37 @@ public class LoginPage extends BasePage {
         clickElement(By.cssSelector(clickRegistration)); // click on הרשמה
 
     }
+    //Click on first name field
+    public void pressTextField(){
+        String firstNameClick= "input[id=ember1237]";
+        clickElement(By.cssSelector(firstNameClick));
+    }
     public void fillFirstName(){
         String firstName= "input[id=ember1237]";
         sendKeysToElement(By.cssSelector(firstName), "Ana");
+        WebElement nameToAssert= new  WebElement();{
+            nameToAssert="Ana";
+            Assert.assertEquals(nameToAssert.getText(), nameToAssert);
+        }
     }
     // fill mail
     public void fillUserEmail() {
         String userMail = "input[data-parsley-type=email]";
         sendKeysToElement(By.cssSelector(userMail), "ana.nisanzon@gmail.com");
+        Assert.assertEquals(userMail, "ana.nisanzon@gmail.com");
+
     }
     // fill password
     public void fillUserPassword() {
         String userPassword = "input[id=valPass]";
         sendKeysToElement(By.cssSelector(userPassword), "Shiur011");
+        Assert.assertEquals(userPassword,"Shiur011");
     }
     //fill assure password
     public void fillAssurePassword() {
         String userAssurePassword = "input[id=ember1243]";
         sendKeysToElement(By.cssSelector(userAssurePassword), "Shiur011");
+        Assert.assertEquals(userAssurePassword,"Shiur011");
     }
     //submit registration to BuyMe
     public void clickSubmit() {
