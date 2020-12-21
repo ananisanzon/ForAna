9067e9eb3c7edb703ee374e51a5619ef2174ae09
ananisanzon <ana.nisanzon@gmail.com>
2020-12-21 20:23:12 +0200

    java.lang.AssertionError: expected [Ana] but found []

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -14,11 +14,13 @@ public class HomePage extends BasePage {
     public void chooseShum()  {
         String shum= "סכום";
         clickElement(By.linkText(shum));
-       //Select shumInShekels= new Select(driver.findElement(By.linkText(shum)));
-        //shumInShekels.selectByVisibleText("<span>200-299 ש\"ח</span>");
+      driver.findElements(By.className("chosen-single")).get(2).click();
 
-        String shumInShekels= "<span>200-299 ש\"ח</span>";
-         clickElement(By.xpath(shumInShekels));
+   // driver.findElements(By.xpath("<span>100-199 ש\"ח</span>")).get(1).click();
+        //Select shumInShekels= new Select(driver.findElement(By.linkText(shum)));
+        //shumInShekels.selectByVisibleText("<span>200-299 ש\"ח</span>");
+      //  String shumInShekels= "<span>200-299 ש\"ח</span>";
+      // driver.findElement(By.xpath(shum)).get(2).;
        // Select selectShum=new Select(driver.findElement(By.linkText(shum)));
        // selectShum.selectByVisibleText(1);
 
